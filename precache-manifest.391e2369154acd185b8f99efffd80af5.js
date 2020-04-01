20d92f6e8c23417f14011c17c44e1a782128a55b
Shilpa Soni <shilpassoni@gmail.com>
2020-04-02 00:00:25 +0530

    Updates

---
+++ precache-manifest.391e2369154acd185b8f99efffd80af5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4af77f8fbc933a039e711ba420b068c",
+    "url": "/hello-world/index.html"
+  },
+  {
+    "revision": "e0638d79e9c2fba32565",
+    "url": "/hello-world/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7aee1260942f3db267dd",
+    "url": "/hello-world/static/js/2.76d229fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello-world/static/js/2.76d229fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0638d79e9c2fba32565",
+    "url": "/hello-world/static/js/main.a27d6afd.chunk.js"
+  },
+  {
+    "revision": "418c32f10a6a563ae081",
+    "url": "/hello-world/static/js/runtime-main.4fe9cb21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-world/static/media/logo.5d5d9eef.svg"
+  }
+]);
